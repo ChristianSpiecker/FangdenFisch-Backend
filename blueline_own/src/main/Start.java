8dae74f4951d96854c22acc5be8945afc52044fc
apadb001 <apadb001@pc18-109.FBI-LX>
2017-01-12 16:37:07 +0100

    test

--- blueline_own/src/main/Start.java
+++ blueline_own/src/main/Start.java
@@ -12,7 +12,7 @@ public class Start {
 		//testeNLP(); 
 		// Zum Test des NLP's auskommentieren
 		
-		System.out.println("asdsa");
+		System.out.println("asdsaaa");
 		try {
 			Controller controller = new Controller();
 		} catch (IOException e) {
